349adf50c0fa0ef0683e8ea8def1a06623ead906
Hassan Kibirige <has2k1@gmail.com>
2016-07-16 23:48:12 -0500

    Fix tag checking

--- deploy_documentation.sh
+++ deploy_documentation.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-set -o xtrace  # Print command traces before executing command
+
+# Exit with nonzero exit code if anything fails
+set -e
+
+# Print command traces before executing command
+set -o xtrace
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
@@ -20,30 +24,32 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist
-# yet (should only happen on first deply)
+# Clone the existing gh-pages for this repo into a directory/
+# Or create a new empty branch if gh-pages doesn't exist yet
 git clone --depth=3 --branch=$TARGET_BRANCH $SSH_REPO $DOC_REPO_DIRECTORY || true
 if [[ -d $DOC_REPO_DIRECTORY ]]; then
-   FIRST_COMMIT=false
+   FIRST_DEPLOY=false
    cd $DOC_REPO_DIRECTORY
 else
    git clone -l -s -n . $DOC_REPO_DIRECTORY
-   FIRST_COMMIT=true
+   FIRST_DEPLOY=true
    cd $DOC_REPO_DIRECTORY
    git checkout --orphan $TARGET_BRANCH
    git reset --hard
 fi
 
+# Copy the html and create .nojekyll file so that github pages
+# doesn't ignore files and folders that start with an underscore.
 cp -a "../$HTML_DIRECTORY/." ./
+touch .nojekyll
 
-# Now let's go have some fun with the cloned repo
+# Commit credentials
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-ls -l
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [[ ! $FIRST_COMMIT ]] &&
+# If there are no changes to the compiled out
+# (e.g. this is a README update) then just bail.
+if [[ ! $FIRST_DEPLOY ]] &&
    [[ -z `git diff --exit-code` ]]; then
     echo "No changes to the output on this push; exiting."
     exit 0
@@ -54,7 +60,8 @@ fi
 git add .
 git commit -m "Documentation: ${TRAVIS_TAG}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# Get the deploy key by using Travis's stored variables to
+# decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
@@ -67,5 +74,6 @@ ssh-add ../deploy_key
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
 
+# Clean up the repository
 cd ..
 rm -rf deploy_key $DOC_REPO_DIRECTORY
